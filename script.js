0acab6118435f99bb9874317ad487fa40f63593b
khoine2701 <khoine2701@gmail.com>
2024-10-30 19:52:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Anh iu bé ,Anh hứa hongg làm bé buồn nữa đouuu :3";
+  titleElement.innerHTML = "Biết Điều Đó Nhóc";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -42,12 +42,12 @@ function resizeYesButton() {
 
 function generateMessage(noCount) {
   const messages = [
-    "Không Bao Giờ",
-    "Anh bicc lỗi rồi ạa",
-    "Mong bé tha lỗi choo anhh :((",
-    "Anhh saii rồi , anhh đáng trách ạ",
-    "Bé đừng giận anhh nữa nhoo",
-    "Anhhh iu bé nhắm nhunnn đóoooo",
+    "Cho m nói lại",
+    "Đm con chó này , chọn lại",
+    "Kí đầu m giờ",
+    "con cặc ba mày",
+    "cho m chọn lại lần cuối",
+    "Con đĩ chó",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
